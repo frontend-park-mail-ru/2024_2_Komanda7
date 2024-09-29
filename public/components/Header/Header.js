7d0d1dd1db6e2e437e6bd378f8f1ccceb9c98edc
Ko71k <33371176+Ko71k@users.noreply.github.com>
2024-09-29 17:07:23 +0300

    Update Header.js
    Now has two states and login/signup does not refresh the page

--- public/components/Header/Header.js
+++ public/components/Header/Header.js
@@ -1,5 +1,5 @@
 export class Header {
-    renderHeader(userIsLoggedIn, logout) {
+    renderHeader(userIsLoggedIn, logout, navigate) {
         const headerElement = document.createElement('header');
 
         // Логотип
@@ -23,14 +23,16 @@ export class Header {
             //User is not logged in
       const btnLogin = document.createElement('button');
       const btnRegister = document.createElement('button');
-      btnLogin.addEventListener('click', function() {
-        window.location.href = '/login'; 
+      btnLogin.addEventListener('click', (event) => {
+        event.preventDefault();
+        const path = '/login';
+        navigate(path);
       });
-      btnRegister.addEventListener('click', function() {
-        window.location.href = '/signup';
-      });
-      
-      btnRegister.setAttribute('href', '/register');
+      btnRegister.addEventListener('click', (event) => {
+        event.preventDefault();
+        const path = '/signup';
+        navigate(path);
+        });
 
       btnLogin.className = "btnLogin"
       btnRegister.className = "btnRegister"
@@ -44,7 +46,13 @@ export class Header {
       const profileLink = document.createElement('a');
       profileLink.href = '/profile';
       const avatarImage = document.createElement('img');
-      avatarImage.src = 'avatar-image.png'; // replace with your avatar image path
+      avatarImage.width = 50; // set width to 50px
+        avatarImage.height = 50; // set height to 50px
+      avatarImage.src = '/static/images/myavatar.png'; // replace with your avatar image path
+      avatarImage.onerror = function() {
+        this.src = "/static/images/default_avatar.png";
+        this.style.objectFit = 'fill';
+      };
       avatarImage.alt = 'Avatar';
       avatarImage.className = 'avatar'; // add a class to style the avatar image
       profileLink.appendChild(avatarImage);
@@ -56,9 +64,7 @@ export class Header {
         try {
           const response = await fetch('http://localhost:8080/logout', {
             method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
+            credentials: 'include',
           });
           if (!response.ok) {
             throw new Error(response.statusText);
@@ -67,7 +73,6 @@ export class Header {
           // redirect to login page or clear the session
         } catch (error) {
           console.error(error);
-          logout();
         }
       };
       buttons.appendChild(logoutButton);
