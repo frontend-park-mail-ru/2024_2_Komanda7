1e4d9643a5c3de195096e3325bcbf71cac1b6023
kataamb <51970143+kataamb@users.noreply.github.com>
2024-09-30 13:39:56 +0300

    Update Header.js

--- public/components/Header/Header.js
+++ public/components/Header/Header.js
@@ -21,46 +21,46 @@ export class Header {
 
         if (!userIsLoggedIn) {
             //User is not logged in
-      const btnLogin = document.createElement('button');
-      const btnRegister = document.createElement('button');
-      btnLogin.addEventListener('click', (event) => {
-        event.preventDefault();
-        const path = '/login';
-        navigate(path);
-      });
-      btnRegister.addEventListener('click', (event) => {
+            const btnLogin = document.createElement('button');
+            const btnRegister = document.createElement('button');
+            btnLogin.addEventListener('click', (event) => {
+            event.preventDefault();
+            const path = '/login';
+            navigate(path);
+        });
+        btnRegister.addEventListener('click', (event) => {
         event.preventDefault();
         const path = '/signup';
         navigate(path);
         });
 
-      btnLogin.className = "btnLogin"
-      btnRegister.className = "btnRegister"
+        btnLogin.className = "btnLogin"
+        btnRegister.className = "btnRegister"
       
-      btnLogin.textContent = "Войти"
-      btnRegister.textContent = "Зарегистрироваться"
-      buttons.appendChild(btnLogin);
-      buttons.appendChild(btnRegister);
-    } else {
+        btnLogin.textContent = "Войти"
+        btnRegister.textContent = "Зарегистрироваться"
+        buttons.appendChild(btnLogin);
+        buttons.appendChild(btnRegister);
+      } else {
         //User is logged in
-      const profileLink = document.createElement('a');
-      profileLink.href = '/profile';
-      const avatarImage = document.createElement('img');
-      avatarImage.width = 50; // set width to 50px
+        const profileLink = document.createElement('a');
+        profileLink.href = '/profile';
+        const avatarImage = document.createElement('img');
+        avatarImage.width = 50; // set width to 50px
         avatarImage.height = 50; // set height to 50px
-      avatarImage.src = '/static/images/myavatar.png'; // replace with your avatar image path
-      avatarImage.onerror = function() {
-        this.src = "/static/images/default_avatar.png";
-        this.style.objectFit = 'fill';
-      };
-      avatarImage.alt = 'Avatar';
-      avatarImage.className = 'avatar'; // add a class to style the avatar image
-      profileLink.appendChild(avatarImage);
-      buttons.appendChild(profileLink);
+        avatarImage.src = '/static/images/myavatar.png'; // replace with your avatar image path
+        avatarImage.onerror = function() {
+          this.src = "/static/images/default_avatar.png";
+          this.style.objectFit = 'fill';
+        };
+        avatarImage.alt = 'Avatar';
+        avatarImage.className = 'avatar'; // add a class to style the avatar image
+        profileLink.appendChild(avatarImage);
+        buttons.appendChild(profileLink);
 
-      const logoutButton = document.createElement('button');
-      logoutButton.textContent = 'Logout';
-      logoutButton.onclick = async () => {
+        const logoutButton = document.createElement('button');
+        logoutButton.textContent = 'Logout';
+        logoutButton.onclick = async () => {
         try {
           const response = await fetch('http://localhost:8080/logout', {
             method: 'POST',
