c0151f379b06cdbb0347e3783ee26426b4687e2a
Ksu <k.kumova@mail.ru>
2024-10-02 12:11:04 +0300

    register fixes

--- public/index.js
+++ public/index.js
@@ -16,40 +16,39 @@ const root = document.getElementById('root');
 
 let userIsLoggedIn = false;
 try {
-  const checkSession = await fetch(`${endpoint}/session`, {
-    method: "GET",
-    headers: {},
-    credentials: 'include',
-    
-  });
-  if (checkSession.ok) {
-    userIsLoggedIn = true;
-  }
-}
-catch {
-  userIsLoggedIn = false;
+    const checkSession = await fetch(`${endpoint}/session`, {
+        method: "GET",
+        headers: {},
+        credentials: 'include',
+
+    });
+    if (checkSession.ok) {
+        userIsLoggedIn = true;
+    }
+} catch {
+    userIsLoggedIn = false;
 }
 
 function logout() {
-  userIsLoggedIn = false;
-  updateLinksContainer();
+    userIsLoggedIn = false;
+    updateLinksContainer();
 }
 
 // Updates the URL when the user navigates
 const navigate = (path) => {
-  window.history.pushState({}, '', path);
-  window.dispatchEvent(new PopStateEvent('popstate'));
+    window.history.pushState({}, '', path);
+    window.dispatchEvent(new PopStateEvent('popstate'));
 };
 
 function updateLinksContainer() {
-  const newHeaderElement = new Header().renderHeader(userIsLoggedIn, logout, navigate);
-  root.replaceChild(newHeaderElement, header);
-  header = newHeaderElement;
+    const newHeaderElement = new Header().renderHeader(userIsLoggedIn, logout, navigate);
+    root.replaceChild(newHeaderElement, header);
+    header = newHeaderElement;
 }
 
 let header = new Header().renderHeader(userIsLoggedIn, logout, navigate);
 root.appendChild(header);
-const nav = new Nav().renderNav()  ;
+const nav = new Nav().renderNav();
 root.appendChild(nav);
 
 //create a feed element
@@ -58,41 +57,40 @@ root.appendChild(newsFeed);
 //load events from backend
 const feedContent = document.createElement('content');
 const fetchFeed = await fetch(`${endpoint}/events`, {
-  method: "GET",
-  headers: {
-      //"Content-Type": "application/json",
-  },
+    method: "GET",
+    headers: {
+        //"Content-Type": "application/json",
+    },
 });
 if (fetchFeed.ok) {
-  const feed = await fetchFeed.json();
-  feedContent.id = 'feedContent';
+    const feed = await fetchFeed.json();
+    feedContent.id = 'feedContent';
 
-  Object.entries(feed).forEach(([key, {description, image}]) => {
-    const FeedElement = document.createElement('div');
-    FeedElement.className = 'feed-element';
-  
-    const imageElement = document.createElement('img');
-    imageElement.src = image;
-    imageElement.onerror = function() {
-      this.src = "/static/images/placeholder.png";
-      this.style.objectFit = 'fill';
-    };
-    FeedElement.appendChild(imageElement);
-  
-    const descriptionElement = document.createElement('div');
-    descriptionElement.className = 'description';
-    descriptionElement.textContent = description;
-    FeedElement.appendChild(descriptionElement);
-  
-    feedContent.appendChild(FeedElement);
-  });
-newsFeed.appendChild(feedContent);
+    Object.entries(feed).forEach(([key, { description, image }]) => {
+        const FeedElement = document.createElement('div');
+        FeedElement.className = 'feed-element';
+
+        const imageElement = document.createElement('img');
+        imageElement.src = image;
+        imageElement.onerror = function() {
+            this.src = "/static/images/placeholder.png";
+            this.style.objectFit = 'fill';
+        };
+        FeedElement.appendChild(imageElement);
+
+        const descriptionElement = document.createElement('div');
+        descriptionElement.className = 'description';
+        descriptionElement.textContent = description;
+        FeedElement.appendChild(descriptionElement);
+
+        feedContent.appendChild(FeedElement);
+    });
+    newsFeed.appendChild(feedContent);
 
 
-}
-else {
-  const errorText = await fetchFeed.json();
-  console.log(errorText);
+} else {
+    const errorText = await fetchFeed.json();
+    console.log(errorText);
 }
 
 //create a footer element
@@ -104,206 +102,202 @@ responseElement.id = 'response';
 
 // Update the routes to render the login and signup forms
 const routes = {
-  '/login': () => {
-    const loginForm = new LoginForm("loginForm");
-    const loginFormElement = loginForm.render();
-    newsFeed.innerHTML = '';
-    newsFeed.appendChild(loginFormElement)
+    '/login': () => {
+        const loginForm = new LoginForm("loginForm");
+        const loginFormElement = loginForm.render();
+        newsFeed.innerHTML = '';
+        newsFeed.appendChild(loginFormElement)
 
 
-    loginFormElement.addEventListener('submit', async function(event) {
-      event.preventDefault(); 
+        loginFormElement.addEventListener('submit', async function(event) {
+            event.preventDefault();
 
-      document.getElementById('loginUsernameError').innerText = '';
-      document.getElementById('loginPasswordError').innerText = '';
-      document.getElementById('loginServerError').innerText = '';
+            document.getElementById('loginUsernameError').innerText = '';
+            document.getElementById('loginPasswordError').innerText = '';
+            document.getElementById('loginServerError').innerText = '';
 
-      const username = removeDangerous(document.getElementById('loginUsernameEntry').value);
-      const password = removeDangerous(document.getElementById('loginPasswordEntry').value);
-
-
-      if (!username) {
-        document.getElementById('loginUsernameError').innerText = EMPTY_FIELD;
-        if (!password) {
-          document.getElementById('loginPasswordError').innerText = EMPTY_FIELD;
-        }
-        return;
-      }
-
-      if (!isValidUsername(username))
-      {
-        document.getElementById('loginUsernameError').innerText = INCORRECT_USERNAME;
-        return;
-      }
-
-      if (!isValidPassword(password))
-      {
-        document.getElementById('loginPasswordError').innerText = INCORRECT_PASSWORD;
-        return;
-      }
+            const username = removeDangerous(document.getElementById('loginUsernameEntry').value);
+            const password = removeDangerous(document.getElementById('loginPasswordEntry').value);
 
 
-      try {
-          //backend request
-          const response = await fetch(`${endpoint}/login`, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ username, password }),
-              credentials: 'include',
-          });
-          const clonedResponse = response.clone();
-          const errorText = await response.text();
-          console.log(errorText);
-          if (errorText === "Alredy logged in") {
-            userIsLoggedIn = true;
-            updateLinksContainer();
-          }
+            if (!username) {
+                document.getElementById('loginUsernameError').innerText = EMPTY_FIELD;
+                if (!password) {
+                    document.getElementById('loginPasswordError').innerText = EMPTY_FIELD;
+                }
+                return;
+            }
 
-          const data = await clonedResponse.json()
-          document.getElementById('response').innerText = data.message;
+            if (!isValidUsername(username)) {
+                document.getElementById('loginUsernameError').innerText = INCORRECT_USERNAME;
+                return;
+            }
 
-          if (!response.ok) {
+            if (!isValidPassword(password)) {
+                document.getElementById('loginPasswordError').innerText = INCORRECT_PASSWORD;
+                return;
+            }
 
-              throw new Error(data.message);
-          }
-          else {
-            userIsLoggedIn = true;
-            document.getElementById('response').innerText = "Вход выполнен";
-            updateLinksContainer();
-            navigate("exhibitions");
-          }
-      } catch (error) {
-        document.getElementById('loginServerError').innerText = 'Неверный логин или пароль';
 
-      }
-    });
-    newsFeed.appendChild(responseElement);
-  },
+            try {
+                //backend request
+                const response = await fetch(`${endpoint}/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ username, password }),
+                    credentials: 'include',
+                });
+                const clonedResponse = response.clone();
+                const errorText = await response.text();
+                console.log(errorText);
+                if (errorText === "Alredy logged in") {
+                    userIsLoggedIn = true;
+                    updateLinksContainer();
+                }
 
-  '/signup': () => {
-    const registerForm = new RegisterForm('registerForm');
-    const registerFormElement = registerForm.render();
-    newsFeed.innerHTML = '';
-    newsFeed.appendChild(registerFormElement);
-    
-    registerFormElement.addEventListener('submit', async function(event) {
-      event.preventDefault(); 
+                const data = await clonedResponse.json()
+                document.getElementById('response').innerText = data.message;
 
-      document.getElementById('registerUsernameError').innerText = '';
-      document.getElementById('registerPasswordError').innerText = '';
-      document.getElementById('registerEmailError').innerText = '';
-      document.getElementById('registerServerError').innerText = '';
-      
+                if (!response.ok) {
 
-      const username = removeDangerous(document.getElementById('registerUsernameEntry').value);
-      const email = removeDangerous(document.getElementById('registerEmailEntry').value);
-      const password = removeDangerous(document.getElementById('registerPasswordEntry').value);
+                    throw new Error(data.message);
+                } else {
+                    userIsLoggedIn = true;
+                    document.getElementById('response').innerText = "Вход выполнен";
+                    updateLinksContainer();
+                    navigate("exhibitions");
+                }
+            } catch (error) {
+                document.getElementById('loginServerError').innerText = 'Неверный логин или пароль';
 
-      if (!username) {
-        document.getElementById('registerUsernameError').innerText = EMPTY_FIELD;
-        if (!password) {
-          document.getElementById('registerEmailError').innerText = EMPTY_FIELD;
-          if (!email) {
-            document.getElementById('registerPasswordError').innerText = EMPTY_FIELD;
-          }
-        }
-        return;
-      }
+            }
+        });
+        newsFeed.appendChild(responseElement);
+    },
 
-      if (!isValidUsername(username))
-      {
-        document.getElementById('registerUsernameError').innerText = INCORRECT_USERNAME;
-        return;
-      }
+    '/signup': () => {
+        const registerForm = new RegisterForm('registerForm');
+        const registerFormElement = registerForm.render();
+        newsFeed.innerHTML = '';
+        newsFeed.appendChild(registerFormElement);
 
-      if (!isValidEmail(email))
-      {
-        document.getElementById('registerEmailError').innerText = INCORRECT_EMAIL;
-        return;
-      }
+        registerFormElement.addEventListener('submit', async function(event) {
+            event.preventDefault();
 
-      if (!isValidPassword(password))
-        {
-          document.getElementById('registerPasswordError').innerText = INCORRECT_PASSWORD;
-          return;
-        }
-  
+            document.getElementById('registerUsernameError').innerText = '';
+            document.getElementById('registerPasswordError').innerText = '';
+            document.getElementById('registerEmailError').innerText = '';
+            document.getElementById('registerServerError').innerText = '';
 
-      try {
-          //backend request
-          const response = await fetch(`${endpoint}/register`, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ username, email, password }),
-          });
-          const clonedResponse = response.clone();
-          const errorText = await response.text();
-          console.log(errorText);
 
-          const data = await clonedResponse.json();
-          document.getElementById('response').innerText = data.message;
-          
-          if (!response.ok) {
-              throw new Error(data.message);
-          }
-          userIsLoggedIn = true;
-          document.getElementById('response').innerText = "Вход выполнен";
-          updateLinksContainer();
-          navigate("exhibitions");
-          //go to 
-      } catch (error) {
-        document.getElementById('registerServerError').innerText = 'Пользователь с такими данными уже существует';
-      }
-    });
-    newsFeed.appendChild(responseElement);
+            const username = removeDangerous(document.getElementById('registerUsernameEntry').value);
+            const email = removeDangerous(document.getElementById('registerEmailEntry').value);
+            const password = removeDangerous(document.getElementById('registerPasswordEntry').value);
 
-  },
-  //load the events
-  '/exhibitions': () => {
-    newsFeed.innerHTML = ''; // Clear the modal window content
-    newsFeed.appendChild(feedContent);
-  },
+            if (!username) {
+                document.getElementById('registerUsernameError').innerText = EMPTY_FIELD;
+                if (!password) {
+                    document.getElementById('registerEmailError').innerText = EMPTY_FIELD;
+                    if (!email) {
+                        document.getElementById('registerPasswordError').innerText = EMPTY_FIELD;
+                    }
+                }
+                return;
+            }
+
+            if (!isValidUsername(username)) {
+                document.getElementById('registerUsernameError').innerText = INCORRECT_USERNAME;
+                return;
+            }
+
+            if (!isValidEmail(email)) {
+                document.getElementById('registerEmailError').innerText = INCORRECT_EMAIL;
+                return;
+            }
+
+            if (!isValidPassword(password)) {
+                document.getElementById('registerPasswordError').innerText = INCORRECT_PASSWORD;
+                return;
+            }
+
+
+            try {
+                //backend request
+                const response = await fetch(`${endpoint}/register`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ username, email, password }),
+                    credentials: 'include',
+
+                });
+                const clonedResponse = response.clone();
+                const errorText = await response.text();
+                console.log(errorText);
+
+                const data = await clonedResponse.json();
+                document.getElementById('response').innerText = data.message;
+
+                if (!response.ok) {
+                    throw new Error(data.message);
+                }
+                userIsLoggedIn = true;
+                document.getElementById('response').innerText = "Вход выполнен";
+                updateLinksContainer();
+                navigate("exhibitions");
+                //go to 
+            } catch (error) {
+                document.getElementById('registerServerError').innerText = 'Пользователь с такими данными уже существует';
+            }
+        });
+        newsFeed.appendChild(responseElement);
+
+    },
+    //load the events
+    '/exhibitions': () => {
+        newsFeed.innerHTML = ''; // Clear the modal window content
+        newsFeed.appendChild(feedContent);
+    },
 };
 
 
 // Update the default route to hide the modal window
 const defaultRoute = () => {
-  newsFeed.innerHTML = '';
-  const newsFeedText = document.createElement('p');
-  newsFeedText.textContent = 'No events!';
-  newsFeed.appendChild(newsFeedText);
-  newsFeed.appendChild(responseElement);
+    newsFeed.innerHTML = '';
+    const newsFeedText = document.createElement('p');
+    newsFeedText.textContent = 'No events!';
+    newsFeed.appendChild(newsFeedText);
+    newsFeed.appendChild(responseElement);
 };
 
 //url bar listener
 window.addEventListener('popstate', () => {
-  const path = window.location.pathname;
-  const route = routes[path];
-  if (route) {
-    route();
-  } else {
-    defaultRoute(); // Call the default route if no matching route is found
-  }
+    const path = window.location.pathname;
+    const route = routes[path];
+    if (route) {
+        route();
+    } else {
+        defaultRoute(); // Call the default route if no matching route is found
+    }
 });
 
 // Check the current path when the page is loaded
 const currentPath = window.location.pathname;
 if (currentPath === '/login' || currentPath === '/signup') {
-  const route = routes[currentPath];
-  if (route) {
-    route();
-  }
+    const route = routes[currentPath];
+    if (route) {
+        route();
+    }
 }
 // Add an event listener to the links
 const links = document.querySelectorAll('a');
 links.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent the default behavior of the link click
-    const path = link.getAttribute('href');
-    navigate(path); // Use your client-side routing mechanism to navigate to the new path
-  });
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default behavior of the link click
+        const path = link.getAttribute('href');
+        navigate(path); // Use your client-side routing mechanism to navigate to the new path
+    });
 });
