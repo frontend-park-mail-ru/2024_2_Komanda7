614d24b903dc608d16507060d313bafe35e4c666
Ko71k <rassmagin.wolf@mail.ru>
2024-12-16 18:11:53 +0300

    logo fix

--- public/src/components/EventContentPage/EventContentPage.js
+++ public/src/components/EventContentPage/EventContentPage.js
@@ -4,8 +4,8 @@ import { navigate } from "../../modules/router.js";
 import locationIcon from '../../assets/images/location.png';
 import placeholderImage from '../../assets/images/placeholder.png';
 
-import vkIcon from '../../assets/icons/vklogo.png';
-import tgIcon from '../../assets/icons/tglogo.png';
+import vkIcon from '../../assets/images/vklogo.png';
+import tgIcon from '../../assets/images/tglogo.png';
 
 export class EventContentPage {
     constructor(eventId) {
@@ -197,13 +197,16 @@ export class EventContentPage {
         eventShare.className = 'event__actions';
 
         // Кнопка "Поделиться ВКонтакте"
-        const vkShareButton = document.createElement('button');
+        const vkShareButton = document.createElement('div');
         vkShareButton.className = 'buttonIcon'; // Используем общий класс для кнопок с иконками
         const vkShare = document.createElement('img');
+        console.log(vkIcon);
+        console.log(vkShare);
         vkShare.src = vkIcon; // Укажите путь к иконке ВКонтакте
-        vkShare.alt = 'Поделиться ВКонтакте';
+        // vkShare.alt = 'Поделиться ВКонтакте';   
         vkShare.className = 'icon'; // Применяем стили для иконки
-        vkShareButton.appendChild(vkIcon);
+        vkShareButton.appendChild(vkShare); // Добавляем иконку ВКонтакте в кнопку
+        vkShareButton.appendChild(document.createTextNode(' Поделиться ВКонтакте')); // Добавляем текст к кнопке
         vkShareButton.addEventListener("click", () => {
             const shareUrl = `https://vk.com/share.php?url=${encodeURIComponent(window.location.href)}`;
             window.open(shareUrl, '_blank');
@@ -214,19 +217,19 @@ export class EventContentPage {
         eventShareTelegram.className = 'event__actions';
 
         // Кнопка "Поделиться в Телеграм"
-        const telegramShareButton = document.createElement('button');
-        telegramShareButton.className = 'buttonIcon'; // Используем общий класс для кнопок с иконками
-        const telegramIcon = document.createElement('img');
-        telegramIcon.src = tgIcon; // Укажите путь к иконке Телеграма
-        telegramIcon.alt = 'Поделиться в Телеграм';
-        telegramIcon.className = 'icon'; // Применяем стили для иконки
-        telegramShareButton.appendChild(telegramIcon);
-        telegramShareButton.addEventListener("click", () => {
-            const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(window.location.href)}`;
-            window.open(telegramUrl, '_blank');
-        });
-        eventShareTelegram.appendChild(telegramShareButton);
-        eventShare.appendChild(eventShareTelegram);
+        // const telegramShareButton = document.createElement('div');
+        // telegramShareButton.className = 'buttonIcon'; // Используем общий класс для кнопок с иконками
+        // const telegramIcon = document.createElement('img');
+        // telegramIcon.src = tgIcon;
+        // telegramIcon.alt = 'Поделиться в Телеграм';
+        // telegramIcon.className = 'icon'; // Применяем стили для иконки
+        // telegramShareButton.appendChild(telegramIcon);
+        // telegramShareButton.addEventListener("click", () => {
+        //     const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(window.location.href)}`;
+        //     window.open(telegramUrl, '_blank');
+        // });
+        // eventShareTelegram.appendChild(telegramShareButton);
+        // eventShare.appendChild(eventShareTelegram);
 
         const deleteButton = document.createElement('button');
         deleteButton.className = 'buttonDelete';
