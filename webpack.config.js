392fd865abab4f36e9d7e8917921df5938c288c6
Ko71k <rassmagin.wolf@mail.ru>
2024-12-16 20:54:56 +0300

    80

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,7 @@ module.exports = {
     },
     // Удаляем секцию proxy полностью
     compress: true,
-    port: 8000, // Меняем порт на 8000
+    port: 80, // Меняем порт на 8000
     host: '0.0.0.0', // Меняем на localhost
     allowedHosts: 'all',
     historyApiFallback: {
