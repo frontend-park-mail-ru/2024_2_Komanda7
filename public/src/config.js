528af2236ebe1497f459bade00b10236eea27740
Ko71k <rassmagin.wolf@mail.ru>
2024-12-16 20:42:11 +0300

    rtract

--- public/src/config.js
+++ public/src/config.js
@@ -1,4 +1,6 @@
-export const endpoint = "https://vyhodnoy.online/api/";
+// export const endpoint = "https://vyhodnoy.online/api/";
+
+export const endpoint = "http://37.139.40.252:8080/";
 
 // export const endpoint = "http://127.0.0.1:8080";
 
